7795e75ece645688794d608fcf8f33da66765b5d
Wilzon Camilo Liscano Galindo <wliscano@codedesignplus.com>
2025-05-04 20:13:57 -0500

    refactor: uncomment GraphController route and attributes for implementation

--- src/entrypoints/CodeDesignPlus.Net.Microservice.MicrosoftGraph.Rest/Controllers/GraphController.cs
+++ src/entrypoints/CodeDesignPlus.Net.Microservice.MicrosoftGraph.Rest/Controllers/GraphController.cs
@@ -1,8 +1,17 @@
 namespace CodeDesignPlus.Net.Microservice.MicrosoftGraph.Rest.Controllers;
 
-// [Route("api/[controller]")]
-// [ApiController]
-// public class GraphController(IMediator mediator, IMapper mapper) : ControllerBase
-// {
-    
-// }
+[Route("api/[controller]")]
+[ApiController]
+public class GraphController() : ControllerBase
+{
+    /// <summary>
+    /// Hello World endpoint for testing purposes.
+    /// </summary>
+    /// <returns>A simple "Hello World" message.</returns>
+    [HttpGet]
+    public Task<IActionResult> HelloWorld()
+    {
+        var result = new { message = "Hello World" };
+        return Task.FromResult<IActionResult>(Ok(result));
+    }
+}
